d1a3900b8b1b5a7cab2d551c6189a285b1a0359f
Jeffry Monsalud <Jeffrym123@hotmail.com>
2022-03-30 23:25:59 -0500

    Initial commit with Customer table

---
+++ db/migrate/20220331042043_create_customers.rb
@@ -0,0 +1,11 @@
+class CreateCustomers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :customers do |t|
+      t.string :name
+      t.string :address
+      t.string :email_address
+
+      t.timestamps
+    end
+  end
+end
