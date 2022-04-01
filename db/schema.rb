707dead1e46205d0a34840b9cda89f6ce13df2fe
Jeffry Monsalud <Jeffrym123@hotmail.com>
2022-03-31 21:35:52 -0500

    Add pages controller and set home page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_31_042043) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_31_194857) do
+  create_table "categories", force: :cascade do |t|
+    t.string "category_name"
+    t.string "category_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "customers", force: :cascade do |t|
     t.string "name"
     t.string "address"
